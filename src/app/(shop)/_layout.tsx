dd03627f7735c77a030021b8b00435599fddba6c
Vaibhav Gupta <rohitvaibhavgupta@gmail.com>
2024-10-20 11:03:32 +0530

    Initial Commit

---
+++ src/app/(shop)/_layout.tsx
@@ -0,0 +1,52 @@
+import { Tabs } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+function TabBarIcon(props: {
+    name: React.ComponentProps<typeof FontAwesome>['name'];
+    color: string;
+}) {
+    return <FontAwesome size={24} {...props} style={{ color: '#1BC464' }} />
+}
+
+const TabsLayout = () => {
+    return (
+        <SafeAreaView edges={['top']} style={styles.safeArea}>
+            <Tabs screenOptions={{
+                tabBarActiveTintColor: '#1BC464',
+                tabBarInactiveTintColor: 'gray',
+                tabBarLabelStyle: { fontSize: 16 },
+                tabBarStyle: {
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                    paddingTop: 2,
+                },
+                headerShown: false,
+            }}>
+            <Tabs.Screen 
+            name='index' 
+            options={{
+                title: 'Shop',
+                tabBarIcon(props) {
+                    return <TabBarIcon {...props} name='shopping-cart' />;
+                },
+            }} />
+            <Tabs.Screen name='orders' options={{
+                title: 'Orders',
+                tabBarIcon(props) {
+                    return <TabBarIcon {...props} name='book' />;
+                },
+            }} />
+        </Tabs>
+        </SafeAreaView>
+    );
+};
+
+export default TabsLayout;
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
+})
---
+++ src/app/_layout.tsx
@@ -0,0 +1,26 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+    return <Stack>
+        <Stack.Screen 
+            name='(shop)'
+            options={{ headerShown: false, title: 'Shop' }}
+        />
+        <Stack.Screen 
+            name='categories'
+            options={{ headerShown: false, title: 'Categories' }}
+        />
+        <Stack.Screen 
+            name='product'
+            options={{ headerShown: true, title: 'Product' }}
+        />
+        <Stack.Screen 
+            name='cart'
+            options={{ presentation: 'modal', title: 'Shopping Cart' }}
+        />
+        <Stack.Screen
+            name='auth'
+            options={{ headerShown: true }}
+        />
+    </Stack>
+}
