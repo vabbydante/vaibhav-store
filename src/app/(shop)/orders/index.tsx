a84fe30564339bb5ceead816528b8a1afbf2ee61
vabbydante <rohitvaibhavgupta@gmail.com>
2024-10-20 14:08:32 +0530

    Added slug for orders

--- src/app/(shop)/orders/index.tsx
+++ src/app/(shop)/orders/index.tsx
@@ -1,13 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  FlatList,
+  ListRenderItem,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { ORDERS } from "../../../../assets/orders";
+import { Order, OrderStatus } from "../../../../assets/types/order";
+import { Link, Stack } from "expo-router";
+
+const statusDisplayText: Record<OrderStatus, string> = {
+  Pending: 'Pending',
+  Completed: 'Completed',
+  Shipped: 'Shipped',
+  InTransit: 'In Transit',
+};
+
+const renderItem: ListRenderItem<Order> = ({ item }) => (
+  <Link href={`/orders/${item.slug}`} asChild>
+    <Pressable style={styles.orderContainer}>
+      <View style={styles.orderContent}>
+        <View style={styles.orderDetailsContainer}>
+          <Text style={styles.orderItem}>{item.slug}</Text>
+          <Text style={styles.orderDetails}>{item.details}</Text>
+          <Text style={styles.orderDate}>{item.date}</Text>
+        </View>
+        <View style={[styles.statusBadge, styles[`statusBadge_${item.status}`]]}>
+          <Text style={styles.statusText}>
+            {statusDisplayText[item.status]}
+          </Text>
+        </View>
+      </View>
+    </Pressable>
+  </Link>
+);
 
 const Orders = () => {
   return (
-    <View>
-      <Text>Orders</Text>
+    <View style={styles.container}>
+      <Stack.Screen options = {{ title: 'Orders' }} />
+      <FlatList
+        data={ORDERS}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default Orders;
 
@@ -17,53 +57,53 @@ const styles: { [key: string]: any } = StyleSheet.create({
     padding: 16,
   },
   orderContainer: {
-    backgroundColor: '#f8f8f8',
+    backgroundColor: "#f8f8f8",
     padding: 16,
     marginVertical: 8,
     borderRadius: 8,
   },
   orderContent: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   orderDetailsContainer: {
     flex: 1,
   },
   orderItem: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   orderDetails: {
     fontSize: 14,
-    color: '#555',
+    color: "#555",
   },
   orderDate: {
     fontSize: 12,
-    color: '#888',
+    color: "#888",
     marginTop: 4,
   },
   statusBadge: {
     paddingVertical: 4,
     paddingHorizontal: 8,
     borderRadius: 4,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
   },
   statusText: {
     fontSize: 12,
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
   },
   statusBadge_Pending: {
-    backgroundColor: '#ffcc00',
+    backgroundColor: "#ffcc00",
   },
   statusBadge_Completed: {
-    backgroundColor: '#4caf50',
+    backgroundColor: "#4caf50",
   },
   statusBadge_Shipped: {
-    backgroundColor: '#2196f3',
+    backgroundColor: "#2196f3",
   },
   statusBadge_InTransit: {
-    backgroundColor: '#ff9800',
+    backgroundColor: "#ff9800",
   },
 });
